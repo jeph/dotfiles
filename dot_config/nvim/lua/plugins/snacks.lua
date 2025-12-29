54e8d2dda6f5f8024144710ee5cddcacc51387df
jeph <23463082+jeph@users.noreply.github.com>
2025-12-28 22:21:12 -0500

    Configure terminal to open over the editor

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "snacks.nvim",
+    opts = {
+      terminal = {
+        win = {
+          position = "float",
+          border = "rounded",
+          backdrop = 60,
+          height = 0.97,
+          width = 0.97,
+        },
+      },
+    },
+  },
+}
